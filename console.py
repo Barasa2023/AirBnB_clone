9b2dd4b9480a5b25d4196be6329256e2cc8e71ca
Barasa2023 <123577514+Barasa2023@users.noreply.github.com>
2024-05-17 09:37:10 +0300

    Update console.py tp handle more functionalities

---
+++ console.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''Console interpreter'''
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    '''Contains the entry point of the command interpreter'''
+
+    prompt = "(hbnb)"
+
+    def do_quit(self, arg: str):
+        '''Method to exit the interpreter'''
+
+        pass
+
+    # def do_help(self, arg: str):
+    #     '''Print help topic'''
+        
+
+    def do_EOF(self):
+        '''Handle end of file'''
+
+        pass
+
+    def do_create(self, arg):
+        '''Creates a new instance of BaseModel'''
+        if arg is None:
+            print("** class name missing **")
+        else:
+            print("New instance created")
+    
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
