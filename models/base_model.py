e1482686a77f634cabf80f4f2ece552b90690b28
Barasa2023 <123577514+Barasa2023@users.noreply.github.com>
2024-05-17 11:44:47 +0300

    Refactor the base_model.py code

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,9 @@
 '''Base model file'''
 
 from datetime import datetime
+import uuid
+from models import storage
+
 
 class BaseModel:
     '''defines all common attributes/methods for other classes'''
@@ -9,15 +12,20 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         '''Initialize intances'''
 
-        if kwargs is not None and kwargs != {}:
-            if kwargs["created_at"] == "created_at":
-                self.created_at = datetime.now()
-            elif kwargs["updated_at"] == "updated_at":
-                self.updated_at = datetime.now()
-            else:
-                pass
+        #if kwargs is not None and kwargs != {}:
+            # if kwargs["created_at"] == "created_at":
+            #     self.created_at = datetime.now()
+            # elif kwargs["updated_at"] == "updated_at":
+            #     self.updated_at = datetime.now()
+            # else:
+            #     pass
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                setattr(self, key, value)
         else:        
-            self.id = id
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
@@ -33,6 +41,7 @@ class BaseModel:
     
     def save(self):
         '''updates the public instance attribute updated at'''
+        storage.save()
 
         self.updated_at = datetime.now()
     
