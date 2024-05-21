000e3cad064ce996e02c5cdf34ad2fcbfacefe2d
Barasa2023 <123577514+Barasa2023@users.noreply.github.com>
2024-05-21 15:48:15 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,8 +6,8 @@ import os
 
 
 class FileStorage:
-    """Serializes instances to a JSON file and 
-    deserializes JSON file to instances"""
+    """Serializes instances to a JSON file and\
+        deserializes JSON file to instances"""
 
     __file_path = "file.json"
     __objects = {}
